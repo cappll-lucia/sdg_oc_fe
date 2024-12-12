297e88ef3666dcc5d4e2d893dca3962cffb5fa2a
Lucia Cappellini <luciacappll@gmail.com>
2024-12-12 18:40:37 -0300

    add router

--- sdg_oc_fe/src/App.vue
+++ sdg_oc_fe/src/App.vue
@@ -4,16 +4,17 @@ import {Button} from '@/components/ui/button'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <Button variant="destructive" >test</Button>
+  <h1>Hello App!</h1>
+  <p>
+    <strong>Current route path:</strong> {{ $route.fullPath }}
+  </p>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/login">Go to Login</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
