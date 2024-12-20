13fe60b9e62effaa36feff254e253c1c38aa76f6
Lucia <luciacappll@gmail.com>
2024-12-19 23:02:34 -0300

    detalle recetas recetados

---
+++ sdg_oc_fe/src/components/ItemDetalleReceta.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts"> 
+
+import type { RecetaRecetados } from '@/api/entities';
+import { Separator } from '@/components/ui/separator'
+
+
+defineProps<{
+    receta: RecetaRecetados;
+    title: string
+}>();
+
+
+
+</script>
+
+<template>
+    <div class="flex flex-row justify-start items-center">
+        <h1 class="mr-12 text-2xl font-extrabold ">{{ title }}</h1>
+        <div class="flex flex-col ">
+            <div class="flex  h-10 items-center">
+                <p class="font-bold w-20 text-lg">O.D.</p>+
+
+                <p class="font-bold w-12 text-right pr-4">Esf.: </p>
+                <p class="w-10">{{receta.odEsf.toFixed(2)}}</p>
+                <Separator orientation="vertical" class="mx-4" />
+
+                <p class="font-bold w-12 text-right pr-4">Cil.:</p>
+                <p class="w-10">{{receta.odCil.toFixed(2)}}</p>
+                <Separator orientation="vertical" class="mx-4 " />
+
+                <p class="font-bold w-12 text-right pr-4">A:</p>
+                <p class="w-10">{{ receta.odA }} °</p>
+            </div>
+
+            <Separator class="my-4" />
+
+            <div class="flex  h-10 items-center">
+                <p class="font-bold w-20 text-lg">O.I.</p>
+
+                <p class="font-bold w-12 text-right pr-4">Esf.</p>
+                <p class="w-10">{{receta.oiEsf.toFixed(2)}}</p>
+                <Separator orientation="vertical" class="mx-4 " />
+
+
+                <p class="font-bold w-12 text-right pr-4">Cil.</p>
+                <p class="w-10">{{receta.oiCil.toFixed(2)}}</p>
+                <Separator orientation="vertical" class="mx-4 " />
+
+                <p class="font-bold w-12 text-right pr-4">A</p>
+                <p class="w-10">{{receta.oiA}}</p>
+            </div>
+        </div>
+        <Separator orientation="vertical" class="w-20" />
+        <div class="pr-4">
+            <p class=" font-bold w-12 text-center">DNP</p>
+            <p class="w-12 text-center">{{receta.dnp1}}</p>
+        </div>
+
+        <Separator orientation="vertical" class="w-14" />
+        <div class="pr-4">
+            <p class="font-bold w-12 text-center">Φ </p>
+            <p class="w-12 text-center">{{receta.dnp2}}</p>
+        </div>
+    </div>
+
+</template>
--- sdg_oc_fe/src/components/ListadoRecetasRecetados.vue
+++ sdg_oc_fe/src/components/ListadoRecetasRecetados.vue
@@ -4,6 +4,7 @@ import { Separator } from '@/components/ui/separator'
 import { Button } from '@/components/ui/button';
 import { ChevronRightIcon } from '@radix-icons/vue'
 import {ref} from 'vue';
+import ItemDetalleReceta from '@/components/ItemDetalleReceta.vue'
 
 
 const props = defineProps<{
@@ -23,23 +24,76 @@ const selectedRec = ref(props.recetas[0]);
     <div class="panel w-100 flex flew-row h-[100%]">
         <div class="panel-index w-[23%]  p-2 h-[100%]">
             <div v-for="receta in recetas" class="mr-4">
-                <div class="panel-inde-item p-2 h-16 flex flex-row justify-between items-center ">
+                <div class="panel-inde-item px-2 py-6 h-16 flex flex-row justify-between items-center rounded-sm"
+                    :class="{ 'bg-[#d7e5ec]': selectedRec === receta }">
                     <p class="font-light text-sm ">{{ receta.id}}</p>
                     <div class="flex-col  w-[50%]">
                         <p class="font-bold ">{{ receta.fecha.toISOString().split("T")[0] }}</p>
                         <p class="font-light  ">{{receta.tipo}}</p>
                     </div>
-                    <Button variant="outline" size="icon" class="bg-transparent hover:bg-gray-200"
+                    <Button variant="outline" size="icon" class="bg-transparent hover:bg-[#d7e5ec]"
                         @click="()=>{selectedRec=receta; console.log(selectedRec)}">
                         <ChevronRightIcon class="w-4 h-4" />
                     </Button>
                 </div>
-                <Separator class="my-4" />
+                <Separator class="my-2" />
             </div>
         </div>
         <Separator orientation="vertical" />
-        <div class="view w-[75%] h-[100%] px-8 py-4" >
-            {{ selectedRec}}
+        <div class="view w-[75%] h-[100%] px-8">
+            <div class="datos flex flex-col" v-if="selectedRec">
+                <div class="flex flex-row justify-between">
+                    <div class="flex flex-col ">
+                        <span class="text-lg font-bold">Tipo Receta: </span>
+                        <span>{{selectedRec.tipo}}</span>
+                    </div>
+                    <div class="flex flex-col ">
+                        <span class="text-lg font-bold w-[10rem]">Fecha Receta: </span>
+                        <span>{{selectedRec.fecha.toISOString().split("T")[0]}}</span>
+                    </div>
+                </div>
+            </div>
+            <Separator class="my-6" />
+
+            <div v-if="selectedRec?.tipo=='Anteojo Lejos' ">
+                <ItemDetalleReceta :receta="selectedRec" title="Lejos" />
+            </div>
+            <div v-else-if="selectedRec?.tipo == 'Anteojo Cerca'">
+                <ItemDetalleReceta :receta="selectedRec" title="Cerca" />
+            </div>
+            <div v-else-if="selectedRec?.tipo == 'Multifocal'">
+                <ItemDetalleReceta :receta="selectedRec" title="Cerca" />
+                <Separator class="my-8" />
+                <ItemDetalleReceta :receta="selectedRec" title="Lejos" />
+            </div>
+            <div v-else-if="selectedRec?.tipo == 'Bifocal'">
+                <ItemDetalleReceta :receta="selectedRec" title="Cerca" />
+                <Separator class="my-8" />
+                <ItemDetalleReceta :receta="selectedRec" title="Lejos" />
+            </div>
+
+            <Separator class="my-8" />
+
+            <div class="datos flex flex-col" v-if="selectedRec">
+                <div class="flex flex-row justify-between ">
+                    <div class="flex flex-col w-[12rem]">
+                        <span class="text font-bold ">Cristal: </span>
+                        <span>{{ selectedRec.cristal ?? '--' }}</span>
+                    </div>
+                    <div class="flex flex-col  w-[12rem]">
+                        <span class="text font-bold ">Color: </span>
+                        <span>{{ selectedRec.color ?? '---' }}</span>
+                    </div>
+                    <div class="flex flex-col  w-[12rem]">
+                        <span class="text font-bold">Tratamiento: </span>
+                        <span>{{ selectedRec.tratamiento ?? '--' }}</span>
+                    </div>
+                </div>
+                <div class="flex flex-col items-start mt-6">
+                    <span class="text font-bold">Observaciones: </span>
+                    <span>{{ selectedRec.observaciones ?? '--' }}</span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
