daac5e67edbcac3494f65be1dd9f85b7751f1c49
BraidaFac <facundobraida98@gmail.com>
2025-04-25 01:37:26 -0300

    agregar atajo a venta desde cc

--- sdg_oc_fe/src/api/entities/movimiento.ts
+++ sdg_oc_fe/src/api/entities/movimiento.ts
@@ -1,61 +1,65 @@
 import { BaseEntity } from "./entities";
 import { RedDePago, TipoMedioDePagoEnum } from "./mediosDePago";
+import { Venta } from "./venta";
 
 export enum TipoMovimiento {
-  DEVOLUCION = 'DEVOLUCION',
-  VENTA = 'VENTA',
-  PAGO = 'PAGO',
+  DEVOLUCION = "DEVOLUCION",
+  VENTA = "VENTA",
+  PAGO = "PAGO",
 }
 
-export interface Movimiento extends BaseEntity   {
+export interface Movimiento extends BaseEntity {
   fechaMovimiento: Date;
   tipoMovimiento: TipoMovimiento;
   importe: number;
+  venta?: Venta;
 }
 
 export const createMovimientoCtaCte = (_movimiento: {
-  tipoMovimiento: TipoMovimiento| undefined,
-  importe: number, 
-  formaPago: TipoMedioDePagoEnum| undefined,
-  entidadBancaria: string | undefined,
-  redDePago: string | undefined,
-})=>{
-  const tiposNoRedValidation = ['EFECTIVO', 'CUENTA_CORRIENTE'];
-  
+  tipoMovimiento: TipoMovimiento | undefined;
+  importe: number;
+  formaPago: TipoMedioDePagoEnum | undefined;
+  entidadBancaria: string | undefined;
+  redDePago: string | undefined;
+}) => {
+  const tiposNoRedValidation = ["EFECTIVO", "CUENTA_CORRIENTE"];
+
   const tiposValidos = Object.keys(TipoMedioDePagoEnum);
   const redesValidas = Object.keys(RedDePago);
-  
-  const formaPagoValida = _movimiento.formaPago 
-      ? tiposValidos.includes(_movimiento.formaPago)
-      : false;
 
-    const entidadValida = !formaPagoValida || tiposNoRedValidation.includes(_movimiento.formaPago!)
+  const formaPagoValida = _movimiento.formaPago
+    ? tiposValidos.includes(_movimiento.formaPago)
+    : false;
+
+  const entidadValida =
+    !formaPagoValida || tiposNoRedValidation.includes(_movimiento.formaPago!)
       ? true
       : !!_movimiento.entidadBancaria?.trim();
 
-    const redValida = !formaPagoValida || tiposNoRedValidation.includes(_movimiento.formaPago!)
+  const redValida =
+    !formaPagoValida || tiposNoRedValidation.includes(_movimiento.formaPago!)
       ? true
       : !!_movimiento.redDePago && redesValidas.includes(_movimiento.redDePago);
 
-    const importeValido = _movimiento.importe > 0;
-
+  const importeValido = _movimiento.importe > 0;
 
   const isValid = {
-      tipoMovimiento: _movimiento.tipoMovimiento ? Object.keys(TipoMovimiento).includes(_movimiento.tipoMovimiento): false,
-      formaPago: formaPagoValida,
-      entidadBancaria: entidadValida,
-      redDePago: redValida,
-      importe: importeValido
-  }
+    tipoMovimiento: _movimiento.tipoMovimiento
+      ? Object.keys(TipoMovimiento).includes(_movimiento.tipoMovimiento)
+      : false,
+    formaPago: formaPagoValida,
+    entidadBancaria: entidadValida,
+    redDePago: redValida,
+    importe: importeValido,
+  };
   const success = Object.values(isValid).every(Boolean);
-  return {success, isValid};
-}
-
+  return { success, isValid };
+};
 
 export type NewMovimientoType = {
-  tipoMovimiento: TipoMovimiento | undefined,
-  importe: number, 
-  formaPago: TipoMedioDePagoEnum | undefined,
-  entidadBancaria: string | undefined,
-  redDePago: string|undefined
-}
+  tipoMovimiento: TipoMovimiento | undefined;
+  importe: number;
+  formaPago: TipoMedioDePagoEnum | undefined;
+  entidadBancaria: string | undefined;
+  redDePago: string | undefined;
+};
--- sdg_oc_fe/src/api/libs/cuentaCorriente.ts
+++ sdg_oc_fe/src/api/libs/cuentaCorriente.ts
@@ -1,59 +1,70 @@
-import { AxiosError } from 'axios';
-import {http} from '../http';
-import { CuentaCorriente } from '../entities/cuentaCorriente';
-import { NewMovimientoType } from '../entities/movimiento';
+import { AxiosError } from "axios";
+import { CuentaCorriente } from "../entities/cuentaCorriente";
+import { NewMovimientoType } from "../entities/movimiento";
+import { http } from "../http";
 
-interface CtaCteFilers{
-    filtro?: string | null;
-    estado?: string | null;
-    limit?: number | string;
-    offset?: number | string;
+interface CtaCteFilers {
+  filtro?: string | null;
+  estado?: string | null;
+  limit?: number | string;
+  offset?: number | string;
 }
 
+const getAll = async (filters: CtaCteFilers) => {
+  try {
+    const params = new URLSearchParams();
 
+    if (filters.filtro) params.append("filtro", filters.filtro);
+    if (filters.estado) params.append("estado", filters.estado);
 
+    params.append("limit", filters.limit?.toString() || "10");
+    params.append("offset", filters.offset?.toString() || "0");
 
-const getAll = async(filters: CtaCteFilers) => {
-    try{
-        const params = new URLSearchParams();
-
-        if (filters.filtro) params.append("filtro", filters.filtro);
-        if (filters.estado) params.append("estado", filters.estado);
-
-        params.append("limit", filters.limit?.toString() || "10");
-        params.append("offset", filters.offset?.toString() || "0");
-        
-        const url = `/cuenta-corriente?${params.toString()}`;
-        const resp = await http.get(url);
-        return resp.data.data as {items: CuentaCorriente[], nextPage: number|null, previousPage: number|null};
-    } catch (error) {
-        throw error instanceof (AxiosError) ?  new Error(error?.response?.data?.message) : new Error('Algo salió mal');
-    }
-}
-
-const getOneByCliente = async (_idCliente: number) => {
-    try {
-        const resp = await http.get(`/cuenta-corriente/cliente/${_idCliente}`);
-        return resp.data.data as CuentaCorriente;
-    } catch (error) {
-        throw error instanceof (AxiosError) ?  new Error(error?.response?.data?.message) : new Error('Algo salió mal');
-    }
+    const url = `/cuenta-corriente?${params.toString()}`;
+    const resp = await http.get(url);
+    return resp.data.data as {
+      items: CuentaCorriente[];
+      nextPage: number | null;
+      previousPage: number | null;
+    };
+  } catch (error) {
+    throw error instanceof AxiosError
+      ? new Error(error?.response?.data?.message)
+      : new Error("Algo salió mal");
+  }
 };
 
+const getOneByCliente = async (_idCliente: number) => {
+  try {
+    const resp = await http.get(`/cuenta-corriente/cliente/${_idCliente}`);
+    return resp.data.data as CuentaCorriente;
+  } catch (error) {
+    throw error instanceof AxiosError
+      ? new Error(error?.response?.data?.message)
+      : new Error("Algo salió mal");
+  }
+};
 
-const updateSaldo = async(_idCliente: number, _newMovimeinto: NewMovimientoType ) =>{
-        try {
-        const resp = await http.patch(`/cuenta-corriente/cliente/${_idCliente}`, _newMovimeinto);
-        return resp.data.data as CuentaCorriente;
-    } catch (error) {
-        throw error instanceof (AxiosError) ?  new Error(error?.response?.data?.message) : new Error('Algo salió mal');
-    }
-}
-
-
+const updateSaldo = async (
+  _idCliente: number,
+  _newMovimeinto: NewMovimientoType
+) => {
+  try {
+    const resp = await http.patch(
+      `/cuenta-corriente/cliente/${_idCliente}`,
+      _newMovimeinto
+    );
+    return resp.data.data as CuentaCorriente;
+  } catch (error) {
+    throw error instanceof AxiosError
+      ? new Error(error?.response?.data?.message)
+      : new Error("Algo salió mal");
+  }
+};
 
 export const cuentaCorrienteApi = {
-    getAll: (_filters: CtaCteFilers)=> getAll(_filters),
-    getOneByCliente: (_id: number)=> getOneByCliente(_id),
-    updateSaldo: (_id: number, _newMovimeinto: NewMovimientoType )=> updateSaldo(_id, _newMovimeinto),
-}
+  getAll: (_filters: CtaCteFilers) => getAll(_filters),
+  getOneByCliente: (_id: number) => getOneByCliente(_id),
+  updateSaldo: (_id: number, _newMovimeinto: NewMovimientoType) =>
+    updateSaldo(_id, _newMovimeinto),
+};
